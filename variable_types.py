704aa7bb057558fe043a71861a043d08ce60daa3
Jesus M Bustamante <jesus.bustamante@gmail.com>
2023-04-16 19:34:05 -0700

    This is the first commit for the variable_types.py file.

---
+++ variable_types.py
@@ -0,0 +1,20 @@
+# Declare variables of each data type
+integer_variable = 42
+float_variable = 3.14
+string_variable = "Hello, world!"
+boolean_variable = True
+list_variable = [1, 2, 3]
+tuple_variable = (4, 5, 6)
+tuple_variable = (2, 3, 4)
+set_variable = {7, 8, 9}
+dictionary_variable = {"name": "John", "age": 30}
+
+# Print out values and their types
+print("Integer variable:", integer_variable, type(integer_variable))
+print("Float variable:", float_variable, type(float_variable))
+print("String variable:", string_variable, type(string_variable))
+print("Boolean variable:", boolean_variable, type(boolean_variable))
+print("List variable:", list_variable, type(list_variable))
+print("Tuple variable:", tuple_variable, type(tuple_variable))
+print("Set variable:", set_variable, type(set_variable))
+print("Dictionary variable:", dictionary_variable, type(dictionary_variable))
