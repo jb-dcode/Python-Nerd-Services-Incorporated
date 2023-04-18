2ec18b1278349da33a1dec36d2ddeac94c657b4c
fequiros <130945753+fequiros@users.noreply.github.com>
2023-04-17 20:47:26 -0700

    First attempt at Jesus' challenge #1!

---
+++ fern_jesus_challenge_1.py
@@ -0,0 +1,22 @@
+import random
+
+random_number = random.randint(0, 100)
+random_number = 50
+
+min = 0
+max = 100
+
+current_guess = round((max + min) / 2)
+
+while current_guess != random_number:
+    if random_number < current_guess:
+        max = current_guess
+    else:
+        min = current_guess
+    
+    current_guess = round((max + min) / 2)
+
+
+
+print("Guess: ", current_guess)
+print("True: ", random_number)
