ec0db5f22f2fad63bd4f58d2e3cffbd849e36767
fequiros <130945753+fequiros@users.noreply.github.com>
2023-04-21 14:51:48 -0700

    Filled in comments and changed variable names

--- Jesus_solution_For_Fer_Review.py
+++ Jesus_solution_For_Fer_Review.py
@@ -10,32 +10,33 @@ May the odds be on your favor.
 
 import random
 
-#
-rn = random.randint(-29389518293123, 92301239132060)
+# generates a uniformly distributed random integer between those numbers
+random_number = random.randint(-29389518293123, 92301239132060)
 
 
-#
-def s(l, h, rn, i):
-    #
-    cg = (l + h) // 2
-    i += 1
+# uses the midpoint between low and high to exclude half of the remaining possible values in the range per iteration. calls itself until current_guess == random_number
+def s(low, high, random_number, iterations):
+    # googled this because I've never seen // before (kinda cheating, sorry), but basically equivalent to floor((l + h) / 2)
+    # calculates one of the integer midpoints and increments counter for # of iterations
+    current_guess = (low + high) // 2
+    iterations += 1
 
-    #
-    if cg == rn:
-        return cg, i
+    # stops recursion when we've found the correct number, and returns the found value and total number of iterations
+    if current_guess == random_number:
+        return current_guess, iterations
 
-    #
-    elif cg > rn:
-        return s(l, cg - 1, rn, i)
+    # the random number is less than our current guess, so all values >= can be excluded. iterate again with new upper bound.
+    elif current_guess > random_number:
+        return s(low, current_guess - 1, random_number, iterations)
 
-    #
+    # the random number is greater than our curren guess, so all values <= can be excluded. iterate again with new lower bound.
     else:
-        return s(cg + 1, h, rn, i)
+        return s(current_guess + 1, high, random_number, iterations)
 
 
-#
-cg, i = s(-29389518293123, 92301239132060, rn, 0)
+# recursively identifies the random number and counts the number of iterations it took, then sets those values equal to "guess" and "iterations"
+guess, iterations = s(-29389518293123, 92301239132060, random_number, 0)
 
-#
-print("asdfasdfasdf:", rn)
-print("asdfasdfasdf:", i)
+# prints the random number and iteration variables
+print("asdfasdfasdf:", random_number)
+print("asdfasdfasdf:", iterations)
