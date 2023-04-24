dbb3efb08ab1388012857a1e34f1a656e95aae64
fequiros <130945753+fequiros@users.noreply.github.com>
2023-04-24 15:08:47 -0700

    Updated timing code to include my new v3 version

--- timing_for_jesus.py
+++ timing_for_jesus.py
@@ -17,7 +17,7 @@ while current_guess != random_number:
   current_guess = (max + min) // 2
 """
 
-v3 = """
+v3_old = """
 min = -29389518293123
 max = 92301239132060
 random_number = random.randint(min, max)
@@ -46,6 +46,23 @@ def s(low, high, random_number):
 s(-29389518293123, 92301239132060, random_number)
 """
 
+v3_new = """
+min = -29389518293123
+max = 92301239132060
+random_number = random.randint(min, max)
+
+iterations = int(math.log(1 / (max-min), 0.5)) + 1
+for i in range(0, iterations):
+    midpoint = (max + min) // 2
+    if random_number < midpoint:
+        max = midpoint
+    else:
+        min = midpoint
+
+guess = min if min == random_number else max
+"""
+
 print(timeit.timeit(v1, setup, number=1000000))
-#print(timeit.timeit(v3, setup, number=1000000))
-print(timeit.timeit(jv, setup, number=1000000)) # jesus' version
+#print(timeit.timeit(v3_old, setup, number=1000000))
+print(timeit.timeit(jv, setup, number=1000000))
+print(timeit.timeit(v3_new, setup, number=1000000))
