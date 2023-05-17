3049b59215f7318ea9937be907e8eae15755fa31
Jesus M Bustamante <jesus.bustamante@gmail.com>
2023-05-16 20:38:20 -0700

    Adding the Functions Intro File.

---
+++ functions_intro.py
@@ -0,0 +1,12 @@
+def hello(name):
+    print(name)
+
+hello("Jesus")
+
+
+def calculation(first_number, second_number):
+    total_sum = first_number + second_number
+    total_multi = first_number * second_number
+    return total_sum, total_multi
+
+print(calculation(2,3))
